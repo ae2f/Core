a67b699c06d54ca134c8f47ea76de3f50a1c4288
ae2f <mincraft20046@gmail.com>
2025-06-18 06:29:10 +0900

    ci/cd

---
+++ test.sh
@@ -0,0 +1,36 @@
+_c=("-DCMAKE_C_COMPILER=gcc" "-DCMAKE_C_COMPILER=clang" "")
+_cc=("-DCMAKE_CXX_COMPILER=g++" "-DCMAKE_CXX_COMPILER=clang++" "")
+
+makers=("-GNinja" "")
+buildtypes=("Release" "Debug" "MinSizeRel" "RelWithDebInfo")
+
+lstdc=("11" "17" "23")
+lstdcc=("17" "20" "23")
+
+__ae2f_CXX=("ON" "OFF")
+__ae2f_IS_SHARED=("ON" "OFF")
+
+for ((i = 0; i < 3; i++)); do
+	for buildtype in ${buildtypes[@]}; do
+		for maker in ${makers[@]}; do
+			for stdc in ${lstdc[@]}; do
+				for stdcc in ${lstdcc[@]}; do
+					for _ae2f_CXX in ${__ae2f_CXX[@]}; do
+						for _ae2f_IS_SHARED in ${__ae2f_IS_SHARED[@]}; do
+							cmake -S . -B build \
+								-DCMAKE_C_STANDARD=$stdc \
+								-DCMAKE_CXX_STANDARD=$stdcc \
+								$maker ${_c[i]} ${_cc[i]} \
+								-Dae2f_CXX=$_ae2f_CXX \
+								-Dae2f_IS_SHARED=$_ae2f_IS_SHARED
+
+							cmake --build build --config $buildtype
+							ctest --test-dir build -C $buildtype
+							cmake -E remove_directory build
+						done
+					done
+				done
+			done
+		done
+	done
+done
