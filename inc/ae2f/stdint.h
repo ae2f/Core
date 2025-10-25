63980df00350194dcfdad1dcc69803fc7eced98b
ae2f <mincraft20046@gmail.com>
2025-10-26 04:03:45 +0900

    Added unsigned ints
     Changes to be committed:
    	modified:   inc/ae2f/stdint.h

--- inc/ae2f/stdint.h
+++ inc/ae2f/stdint.h
@@ -19,22 +19,43 @@
 #define int8_t	char
 #endif
 
+#ifndef uint8_t
+#define uint8_t	unsigned char
+#endif
+
 #ifndef int16_t
 #define int16_t	short
 #endif
 
-#ifndef int32_t
+#ifndef uint16_t
+#define uint16_t unsigned short
+#endif
 
-#if INT_MAX >> 31
+#ifndef int32_t
+#if (INT_MAX) >> 31
 #define int32_t	int
 #else
 #define int32_t long
 #endif
 #endif
 
+#ifndef uint32_t
+#if (UINT_MAX) >> 31
+#define uint32_t unsigned int
+#else
+#define uint32_t unsigned long
+#endif
+#endif
+
 #ifndef int64_t
-#if LONG_MAX >> 63
+#if (LONG_MAX) >> 63
 #define int64_t	long
+#endif
+#endif
+
+#ifndef uint64_t
+#if (ULONG_MAX) >> 63
+#define uint64_t unsigned long
 #endif
 #endif
 
