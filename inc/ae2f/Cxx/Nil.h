90484d55721dc5637298130ed0ab51d3bcd4d36b
ae2f <mincraft20046@gmail.com>
2025-11-13 01:17:07 +0900

    Changes to be committed: 	modified:   inc/ae2f/Cxx/Nil.h

--- inc/ae2f/Cxx/Nil.h
+++ inc/ae2f/Cxx/Nil.h
@@ -6,5 +6,5 @@
 /** @macro ae2f_NIL @brief NULL for C/C++ */
 #define	ae2f_NIL	ae2f_WhenC(NULL)	ae2f_WhenCXX(nullptr)
 #else
-#define ae2f_NIL	NULL
+#define ae2f_NIL	ae2f_WhenC(NULL)	ae2f_WhenCXX(0)
 #endif
