0c738da2ea1e98566e4fac5766532f191a5f4b6b
ae2f <mincraft20046@gmail.com>
2025-11-13 01:32:16 +0900

    Changes to be committed: 	modified:   inc/ae2f/Cxx/Nil.h

--- inc/ae2f/Cxx/Nil.h
+++ inc/ae2f/Cxx/Nil.h
@@ -1,10 +1,15 @@
 #include "../LangVer.h"
 
 
+
+/** 
+ * @macro ae2f_NIL 
+ * @brief NULL for C/C++ 
+ **/
+
 #undef	ae2f_NIL
 #if __ae2f_stdcheck_CC(201100L)
-/** @macro ae2f_NIL @brief NULL for C/C++ */
 #define	ae2f_NIL	ae2f_WhenC(NULL)	ae2f_WhenCXX(nullptr)
 #else
-#define ae2f_NIL	ae2f_WhenC(NULL)	ae2f_WhenCXX(0)
+#define ae2f_NIL	((void*)0)
 #endif
