22ab4bab1ee12940452e84437f85a9c9d588f020
dalmurii <>
2026-01-16 01:48:45 +0900

    UPDATE - added ae2f_TYPEDEF
     Changes to be committed:
    	modified:   inc/ae2f/Guide.h

--- inc/ae2f/Guide.h
+++ inc/ae2f/Guide.h
@@ -23,14 +23,24 @@
 #define	ae2f_LP(a) ae2f_NONE
 
 /** @def ae2f_opt @brief Optional. */
-#define ae2f_opt ae2f_NONE
+#define ae2f_opt		ae2f_NONE
 
 /** 
  * @def ae2f_FREE 
  * @brief Use this marker to show the desired functions
  * 	to effectively free the resources.
  * */
-#define ae2f_FREE(a) ae2f_NONE
+#define ae2f_FREE(a)		ae2f_NONE
+
+
+/**
+ * @def	ae2f_BITFIELD
+ * @brief	Use this to notify the user that
+ * 		it expects `a` type.
+ *
+ * @details	Useful when standard typedef is not possible.
+ * */
+#define ae2f_TYPEDEF(a)	ae2f_NONE
 
 /** @brief l_value. */
 #define ae2f_v ae2f_NONE
